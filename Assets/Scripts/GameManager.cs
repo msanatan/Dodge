b5e793649f31f0a6e84de063071693844582782a
Marcus Sanatan <msanatan@gmail.com>
2021-07-20 22:42:31 -0400

    Add HUD with score
    We increment the score by 1 every second the player is alive.
    
    Of course, right now there's nothing to make a game over...
    However, the coroutine runs every second and score is incrementing

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+    WaitForSeconds scoreDelay = new WaitForSeconds(1);
+    bool gameOver = false;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("IncrementScore", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator IncrementScore(int score)
+    {
+        while (!gameOver)
+        {
+            score++;
+            scoreText.text = $"Score: {score}";
+            yield return scoreDelay;
+        }
+    }
+}
