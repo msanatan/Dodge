e71e841894d3c1b4cad9073c55ec94bbfe19ea30
Marcus Sanatan <msanatan@gmail.com>
2021-07-25 16:12:31 -0400

    Determine direction of square based on its initial position
    We don't want it quickly exiting the screen from the direction it was spawned from

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    float speed = 80f;
-    [SerializeField] Vector2 direction = new Vector2(-1, 0);
+    private float speed = 80f;
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
+        var sign = transform.position.x < 0 ? 1 : -1;
+        direction = new Vector2(sign, 0);
         GetComponent<Rigidbody2D>().AddForce(direction * speed);
     }
 
